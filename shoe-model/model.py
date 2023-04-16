e9f7a6bbed462ec48b76ee32c00cc0c1050ee5aa
thomasA0509 <tallred2022@fau.edu>
2023-04-16 19:36:26 -0400

    Finished adding images to all categories

--- shoe-model/model.py
+++ shoe-model/model.py
@@ -4,15 +4,15 @@ import os
 from tensorflow.keras import layers
 
 # Define the input shape
-input_shape = (128, 128, 3)
+input_shape = (128, 128, 1)
 
 # Define the number of classes
-num_classes = 3
+num_classes = 29
 
 # Define the directories
-train_dir = os.path.join(os.getcwd(), "cow-natural/Train")
-test_dir = os.path.join(os.getcwd(), "cow-natural/Test")
-val_dir = os.path.join(os.getcwd(), "cow-natural/Val")
+train_dir = os.path.join(os.getcwd(), "shoe-model/train")
+test_dir = os.path.join(os.getcwd(), "shoe-model/test")
+val_dir = os.path.join(os.getcwd(), "shoe-model/val")
 
 # Define the model architecture
 model = keras.Sequential([
@@ -35,13 +35,13 @@ model.summary()
 
 # Define the data generators
 train_data_gen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-train_generator = train_data_gen.flow_from_directory(train_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical')
+train_generator = train_data_gen.flow_from_directory(train_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical', color_mode='grayscale')
 
 val_data_gen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-val_generator = val_data_gen.flow_from_directory(val_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical')
+val_generator = val_data_gen.flow_from_directory(val_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical', color_mode='grayscale')
 
 test_data_gen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
-test_generator = test_data_gen.flow_from_directory(test_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical')
+test_generator = test_data_gen.flow_from_directory(test_dir, target_size=input_shape[:2], batch_size=32, class_mode='categorical', color_mode='grayscale')
 
 # Train the model
 history = model.fit(train_generator, epochs=10, validation_data=val_generator)
@@ -51,6 +51,6 @@ test_loss, test_acc = model.evaluate(test_generator)
 print('Test accuracy:', test_acc)
 
 if (test_acc > 0.95):
-    model.save("cowNaturalModel.h5")
+    model.save("shoeSilhouetteModel.h5")
 else:
   print('Model accuracy is too low to save. Try adding more images to the training set.')
